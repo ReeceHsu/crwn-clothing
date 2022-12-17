387e9ad19b580110c0da9433c462533f5c0a5f91
hsu <viewsonic521@gmail.com>
2022-12-17 13:08:36 +0900

    add input type, import button style

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -39,7 +39,7 @@ const SignUpForm = () => {
 	};
 
 	const handleChange = event => {
-		const { name, value } = event;
+		const { name, value } = event.target;
 
 		setFormFields({ ...formFileds, [name]: value });
 	};
@@ -49,11 +49,19 @@ const SignUpForm = () => {
 			<h2>Don't have an account?</h2>
 			<span>Sign up with your email and password</span>
 			<form onSubmit={handleSubmit}>
-				<FormInput label='Display Name' required onChange={handleChange} name='displayName' value={displayName} />
-				<FormInput label='Email' required onChange={handleChange} name='email' value={email} />
-				<FormInput label='Password' required onChange={handleChange} name='password' value={password} />
+				<FormInput
+					label='Display Name'
+					type='text'
+					required
+					onChange={handleChange}
+					name='displayName'
+					value={displayName}
+				/>
+				<FormInput label='Email' type='email' required onChange={handleChange} name='email' value={email} />
+				<FormInput label='Password' type='password' required onChange={handleChange} name='password' value={password} />
 				<FormInput
 					label='Confirm Password'
+					type='password'
 					required
 					onChange={handleChange}
 					name='confirmPassword'
