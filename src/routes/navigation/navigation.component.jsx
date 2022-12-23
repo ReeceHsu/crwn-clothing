615cfb3ad9ea9ba3674202303f72b88adf52988a
hsu <viewsonic521@gmail.com>
2022-12-23 15:54:55 +0900

    replace userContext with Redux, created action selector type for user in store, remove user.context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,22 @@
 import { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CardDropdown from '../../components/card-dropdown/card-dropdown.component';
 
-import { UserContext } from '../../context/user.context';
+import { selectCurrentUser } from '../../store/user/user.selector';
 import { CartContext } from '../../context/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './navigation.styles';
 
 const Navigation = () => {
-	const { currentUser, setCurrentUser } = useContext(UserContext);
+	const  currentUser = useSelector(selectCurrentUser)
 	const { isCartOpen } = useContext(CartContext);
 	const signOutHandler = async () => {
 		await signOutUser();
-		setCurrentUser(null);
 	};
 	return (
 		<Fragment>
