2bc71238dac8960fc1cceed5679d242178a1e90e
hsu <viewsonic521@gmail.com>
2022-12-31 17:28:35 +0900

    get errer message

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -45,7 +45,7 @@ const PaymentForm = () => {
 		setIsProcessingPayment(false);
 
 		if (paymentResult.error) {
-			alert(paymentResult.error);
+			alert(paymentResult.error.message);
 		} else {
 			if (paymentResult.paymentIntent.status === 'succeeded') {
 				alert('Payment successful');
