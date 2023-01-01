c8780e02dfc6537a8699ad2006f0a4dd38d850f2
hsu <viewsonic521@gmail.com>
2023-01-01 17:38:25 +0900

    typing category reducer and selector

---
+++ src/store/categories/categories.selector.ts
@@ -0,0 +1,23 @@
+import { createSelector } from 'reselect';
+
+import { CategoriesState } from './categories.reducer';
+import { CategoryMap } from './categories.types';
+
+const selectCategoryReducer = (state): CategoriesState => state.categories;
+
+export const selectCategories = createSelector([selectCategoryReducer], categoriesSlice => categoriesSlice.categories);
+
+export const selectrCategoriesMap = createSelector(
+	[selectCategories],
+	(categories): CategoryMap =>
+		categories.reduce((acc, category) => {
+			const { title, items } = category;
+			acc[title.toLowerCase()] = items;
+			return acc;
+		}, {} as CategoryMap),
+);
+
+export const selectCategoriesLoading = createSelector(
+	[selectCategoryReducer],
+	categoriesSlice => categoriesSlice.isLoading,
+);
