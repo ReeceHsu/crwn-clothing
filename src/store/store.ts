77db5c86271361bf57a5caf050450c126e74e6a8
hsu <viewsonic521@gmail.com>
2023-01-03 11:09:24 +0900

    typed rootState

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware, Middleware } from 'redux';
 import logger from 'redux-logger';
 
 import createSagaMiddleware from 'redux-saga';
@@ -7,9 +7,13 @@ import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
+export type RootState = ReturnType<typeof rootReducer>
+
 const sagaMiddleware = createSagaMiddleware();
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger, sagaMiddleware].filter(Boolean);
+const middleWares = [process.env.NODE_ENV === 'development' && logger, sagaMiddleware].filter(
+	(middleware): middleware is Middleware => Boolean(middleware),
+);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,6 +1,8 @@
 import { createSelector } from 'reselect';
+
+import { RootState } from '../store';
 import { UserState } from './user.reducer';
 
-export const currentUserReducer = (state): UserState => state.user.currentUser;
+export const currentUserReducer = (state: RootState): UserState => state.user;
 
-export const selectCurrentUser = createSelector(currentUserReducer, currentUser => currentUser.currentUser);
+export const selectCurrentUser = createSelector(currentUserReducer, user => user.currentUser);
