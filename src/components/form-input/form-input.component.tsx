bdffe184244e017d2e48f417822c9097a8223ba0
hsu <viewsonic521@gmail.com>
2023-01-03 19:10:28 +0900

    typed formInput component

---
+++ src/components/form-input/form-input.component.tsx
@@ -0,0 +1,21 @@
+import { InputHTMLAttributes, FC } from 'react';
+
+import { FormInputLabel, Group, Input } from './form-input.styles';
+
+type FormInputProps = { label: string } & InputHTMLAttributes<HTMLElement>;
+
+const FormInput: FC<FormInputProps> = ({ label, ...otherProps }) => {
+	return (
+		<Group>
+			<Input {...otherProps} />
+			{label && (
+				<FormInputLabel
+					shrink={Boolean(otherProps.value && typeof otherProps.value === 'string' && otherProps.value.length)}>
+					{label}
+				</FormInputLabel>
+			)}
+		</Group>
+	);
+};
+
+export default FormInput;
